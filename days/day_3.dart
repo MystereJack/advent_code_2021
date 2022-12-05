3a1e9e07c18ae08b7d43eff5e60cf2b4d2ee8a1b
Matthieu Rosset <matthieu.rosset@unifr.ch>
2022-12-05 13:11:07 +0100

    day 3 fix

--- days/day_3.dart
+++ days/day_3.dart
@@ -4,13 +4,14 @@ void main(List<String> arguments) async {
   final lines = File('inputs/day_3.txt').readAsStringSync().split('\n').toList();
 
   List<int> sum = lines.fold([], (p, e) {
-    List<int> el = e.split('').map((e1) => int.parse(e1)).toList();
+    List<int> ei = e.split('').map((e) => int.parse(e)).toList();
+
     if (p.isEmpty) {
-      return el;
+      return ei;
     }
 
     for (int i = 0; i < p.length; i++) {
-      p[i] = p[i] + el[i];
+      p[i] = p[i] + ei[i];
     }
 
     return p;
@@ -21,14 +22,51 @@ void main(List<String> arguments) async {
 
   int solution1 = int.parse(gamma, radix: 2) * int.parse(epsilon, radix: 2);
 
+  String oxygen = _searchOxygenC02(lines, true);
+  String co2 = _searchOxygenC02(lines, false);
+
+  int solution2 = int.parse(oxygen, radix: 2) * int.parse(co2, radix: 2);
+
   print('1 : $solution1');
+  print('2 : $solution2');
+}
+
+String _searchOxygenC02(List<String> lines, bool reversed) {
+  List<String> candidates = lines;
+  String s = '';
+  for (int i = 0; i < lines[0].length; i++) {
+    s += _mostCommonDigit(candidates, i, reversed);
+    candidates = lines.where((element) => element.startsWith(s)).toList();
+
+    if (candidates.length == 1) {
+      s = candidates.first;
+      break;
+    }
+  }
+
+  return s;
+}
+
+_mostCommonDigit(List<String> lines, int position, bool reversed) {
+  int count0 = lines.fold(0, (p, e) => e.split('')[position] == '0' ? p + 1 : p + 0);
+  int count1 = lines.fold(0, (p, e) => e.split('')[position] == '1' ? p + 1 : p + 0);
+
+  if (count0 == count1) {
+    return reversed ? '0' : '1';
+  } else if (count0 > count1) {
+    return reversed ? '1' : '0';
+  } else {
+    return reversed ? '0' : '1';
+  }
 }
 
 _calculateGamma(List<int> sum, int length) {
   String result = '';
 
   for (int value in sum) {
-    if (value > length / 2) {
+    if (value == length / 2) {
+      result += '1';
+    } else if (value > length / 2) {
       result += '1';
     } else {
       result += '0';
@@ -40,7 +78,9 @@ _calculateGamma(List<int> sum, int length) {
 _calculateEpsilon(List<int> sum, int length) {
   String result = '';
   for (int value in sum) {
-    if (value > length / 2) {
+    if (value == length / 2) {
+      result += '0';
+    } else if (value > length / 2) {
       result += '0';
     } else {
       result += '1';
