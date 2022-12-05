c63c2c5cf7a6580095e05c596edd2024a8f37fe3
Matthieu Rosset <matthieu.rosset@unifr.ch>
2022-12-05 13:15:47 +0100

    day 3 fix

--- days/day_3.dart
+++ days/day_3.dart
@@ -1,29 +1,15 @@
 import 'dart:io';
 
-void main(List<String> arguments) async {
+void main(List<String> arguments) {
   final lines = File('inputs/day_3.txt').readAsStringSync().split('\n').toList();
 
-  List<int> sum = lines.fold([], (p, e) {
-    List<int> ei = e.split('').map((e) => int.parse(e)).toList();
-
-    if (p.isEmpty) {
-      return ei;
-    }
-
-    for (int i = 0; i < p.length; i++) {
-      p[i] = p[i] + ei[i];
-    }
-
-    return p;
-  });
-
-  String gamma = _calculateGamma(sum, lines.length);
-  String epsilon = _calculateEpsilon(sum, lines.length);
+  String gamma = _calculateGammaEpsilon(lines, false);
+  String epsilon = _calculateGammaEpsilon(lines, true);
 
   int solution1 = int.parse(gamma, radix: 2) * int.parse(epsilon, radix: 2);
 
-  String oxygen = _searchOxygenC02(lines, true);
-  String co2 = _searchOxygenC02(lines, false);
+  String oxygen = _calculateOxygenCO2(lines, false);
+  String co2 = _calculateOxygenCO2(lines, true);
 
   int solution2 = int.parse(oxygen, radix: 2) * int.parse(co2, radix: 2);
 
@@ -31,7 +17,17 @@ void main(List<String> arguments) async {
   print('2 : $solution2');
 }
 
-String _searchOxygenC02(List<String> lines, bool reversed) {
+String _calculateGammaEpsilon(List<String> lines, bool reversed) {
+  String s = '';
+
+  for (int i = 0; i < lines[0].length; i++) {
+    s += _mostCommonDigit(lines, i, reversed);
+  }
+
+  return s;
+}
+
+String _calculateOxygenCO2(List<String> lines, bool reversed) {
   List<String> candidates = lines;
   String s = '';
   for (int i = 0; i < lines[0].length; i++) {
@@ -58,33 +54,4 @@ _mostCommonDigit(List<String> lines, int position, bool reversed) {
   } else {
     return reversed ? '0' : '1';
   }
-}
-
-_calculateGamma(List<int> sum, int length) {
-  String result = '';
-
-  for (int value in sum) {
-    if (value == length / 2) {
-      result += '1';
-    } else if (value > length / 2) {
-      result += '1';
-    } else {
-      result += '0';
-    }
-  }
-  return result;
-}
-
-_calculateEpsilon(List<int> sum, int length) {
-  String result = '';
-  for (int value in sum) {
-    if (value == length / 2) {
-      result += '0';
-    } else if (value > length / 2) {
-      result += '0';
-    } else {
-      result += '1';
-    }
-  }
-  return result;
 }
